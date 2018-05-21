1e5afab2042c2b688fbea52f27e5a7413a5ede8a
Vanessa Toro <vanetoro84@gmail.com>
2018-05-21 22:54:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,9 +16,14 @@ class App < Sinatra::Base
     @num = params[:number].to_i
     @phrase = params[:phrase]
     str = ''
-    @num.times do
-      str += "#{@phrase} "
-    end
+      @num.times do
+        str += "#{@phrase} "
+      end
     "#{str}"
+  end
+
+  get '/:word1/:word2/:word3/:word4/:word5' do
+    @str = params[:word1] + params[:word2] + params[:word3]+ params[:word4] + params[:word5]
+    "#{@str}"
   end
 end
