10fd9b7a0f08a30f7f1ae99ee1f3eb813eb944a0
Vanessa Toro <vanetoro84@gmail.com>
2018-05-21 23:02:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,6 +28,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    binding.pry
     @total = params[:number1].to_i params[:operation] params[:number1].to_i
     "#{@total}"
   end
